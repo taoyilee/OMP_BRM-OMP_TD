Fix numerical sensitivity issue

chain_experiment.py
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     std_error_brm = []
     std_error_lars_td = []
     higher_exp = -1
-    lower_exp = -5
-    beta = np.logspace(higher_exp, lower_exp, 5)
+    lower_exp = -7
+    beta = np.logspace(higher_exp, lower_exp, 25)
     for i, b in enumerate(beta):
         print(f"beta = {b}")
         results = [trial(b, seed) for seed in range(5)]
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     plt.xscale("log")
     plt.xlim(10 ** higher_exp, 10 ** lower_exp)
     plt.xlabel("beta")
-    plt.ylabel("||V* - Vp||")
+    plt.ylabel("||V* - Vbeta||")
     plt.tight_layout()
     plt.savefig(os.path.join(output_dir, "chain.png"))
     plt.legend()

solvers/lars_td.py
@@ -44,10 +44,14 @@ class LARS_TD:
             phi_i = phi[:, I.I]
             phi_i_prime = phi_prime[:, I.I]
             aii = np.matmul(phi_i.T, phi_i - gamma * phi_i_prime)
-            delta_w = np.matmul(np.linalg.inv(aii), np.where(c[I.I] >= 0, 1, -1))
+            try:
+                # delta_w = np.matmul(np.linalg.pinv(aii), np.where(c[I.I] >= 0, 1, -1))
+                delta_w = np.matmul(np.linalg.pinv(aii), np.sign(c[I.I]))
+            except np.linalg.LinAlgError:
+                return w.squeeze(), len(I), np.array(I.I)
             d = np.matmul(np.matmul(phi.T, phi_i - gamma * phi_i_prime), delta_w)
-            left_term, left_i = min_plus((beta_bar - c[I.IBar]) / (d[I.IBar] - 1))
-            right_term, right_i = min_plus((beta_bar + c[I.IBar]) / (d[I.IBar] + 1))
+            left_term, left_i = min_plus((c[I.IBar] - beta_bar) / (d[I.IBar] - 1))
+            right_term, right_i = min_plus((c[I.IBar] + beta_bar) / (d[I.IBar] + 1))
             alpha1, i1 = (left_term, left_i) if left_term < right_term else (right_term, right_i)
             i1 = I.IBar[i1]
 
@@ -57,10 +61,8 @@ class LARS_TD:
             w[I.I], beta_bar, c = w[I.I] + alpha * delta_w, beta_bar - alpha, c - alpha * d
 
             if alpha1 < alpha2:
-                print(f"Add {i1}")
                 I.add(i1)
             else:
-                print(f"Remove {i2}")
                 I.remove(i2)
 
         return w.squeeze(), len(I), np.array(I.I)

test/test_lars_td.py
@@ -15,13 +15,7 @@ if __name__ == "__main__":
     seed = 0
     gamma = 0.8
     n_states = 50
-    mean_error_td = []
-    mean_error_brm = []
-    std_error_td = []
-    std_error_brm = []
-    higher_exp = -1
-    lower_exp = -7
-    beta = 1e-3
+    beta = 0.0000001
 
     print(f"beta = {beta}")
     phi = []
@@ -47,7 +41,7 @@ if __name__ == "__main__":
     w_td, non_zero, selected_indexes = LARS_TD(beta=beta).fit(gamma=gamma, phi=phi, phi_prime=phi_prime,
                                                               reward=reward)
     print(w_td)
-    print(selected_indexes)
+    print(non_zero, selected_indexes)
     print(vstar)
     print(np.matmul(phi_50, w_td))
-    print(np.round(np.abs(vstar - np.matmul(phi_50, w_td)), 2))
+    print(np.sqrt(np.mean((vstar - np.matmul(phi_50, w_td))**2)))

trial.py
@@ -3,6 +3,8 @@
 #  This source code is released under MIT license. Check LICENSE for details.
 
 
+import time
+
 import numpy as np
 
 from problems import ChainMRP
@@ -27,24 +29,44 @@ def trial(beta, seed, n_states=50, gamma=0.8, samples=500):
     phi_prime = np.array(phi_prime)
     reward = np.array(reward)
     phi_50 = np.array([c.rbf.phi(s) for s in range(1, n_states + 1)])
+    start = time.perf_counter_ns()
     w_brm, non_zero_brm, selected_indexes = OMP_BRM(beta=beta).fit(gamma=gamma, phi=phi, phi_prime=phi_prime,
-                                                               reward=reward)
+                                                                   reward=reward)
+    end = time.perf_counter_ns()
+    omp_brm_time = end - start
+
     vbeta_brm = np.matmul(phi_50, w_brm)
     error_brm = np.sqrt(np.mean((vstar - vbeta_brm) ** 2))
+
+    start = time.perf_counter_ns()
     w_td, non_zero_td, selected_indexes = OMP_TD(beta=beta).fit(gamma=gamma, phi=phi, phi_prime=phi_prime,
-                                                             reward=reward)
+                                                                reward=reward)
+    end = time.perf_counter_ns()
+    omp_td_time = end - start
+
     vbeta_td = np.matmul(phi_50, w_td)
     error_td = np.sqrt(np.mean((vstar - vbeta_td) ** 2))
     # lars -TD
+    start = time.perf_counter_ns()
     w_lars_td, non_zero_lars_td, selected_indexes = LARS_TD(beta=beta).fit(gamma=gamma, phi=phi, phi_prime=phi_prime,
-                                                                   reward=reward)
+                                                                           reward=reward)
+    end = time.perf_counter_ns()
+    lars_td_time = end - start
+
     vbeta_laras_td = np.matmul(phi_50, w_lars_td)
     error_lars_td = np.sqrt(np.mean((vstar - vbeta_laras_td) ** 2))
     print(non_zero_brm, non_zero_td, non_zero_lars_td)
+    print(f"{omp_brm_time:.2f}, {omp_td_time:.2f}, {lars_td_time:.2f}")
     return {"beta": beta, "seed": seed,
             "error_td": error_td,
             "error_brm": error_brm,
             "error_lars_td": error_lars_td,
             "vstar": vstar,
             "reward": c._reward,
+            "omp_brm_time": omp_brm_time,
+            "omp_td_time": omp_td_time,
+            "lars_td_time": lars_td_time,
+            "non_zero_brm": non_zero_brm,
+            "non_zero_td": non_zero_td,
+            "non_zero_lars_td": non_zero_lars_td,
             "state_transition": c._transition_matrix}